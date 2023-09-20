ca77b15e351f1513d9fe67f21e3638bf1bfa0602
AkshayPanayada <105909152+AkshayPanayada@users.noreply.github.com>
2023-09-21 01:18:01 +0530

    Update kilonova.py

--- skysurvey/target/kilonova.py
+++ skysurvey/target/kilonova.py
@@ -1,10 +1,139 @@
 from .core import Transient
+
 #This is the code to model kilonova
 import sncosmo as sc
-from simsurvey.utils import model_tools
-from simsurvey.models import AngularTimeSeriesSource
+from scipy.interpolate import (InterpolatedUnivariateSpline as Spline1d,
+                               RectBivariateSpline as Spline2d)
 import numpy as np
 optional_injection_parameters={}
+
+def read_possis_file(filename):
+    """Read in a spectral model created by POSSIS (1906.04205), as appropriate
+       for injestion as a simsurvey.models.AngularTimeSeriesSource. Model
+       grids can be found here: https://github.com/mbulla/kilonova_models.
+    Parameters
+    ----------
+    filename : str
+        Path to the POSSIS file
+    Returns
+    -------
+    phase : `~numpy.ndarray`
+        Phases in days.
+    wave : `~numpy.ndarray`
+        Wavelengths in Angstroms.
+    flux : `~numpy.ndarray`
+        Model spectral flux density in arbitrary units.
+        Must have shape `(num_phases)`.
+    cos_theta : `~numpy.ndarray`
+        cosine of viewing angle
+    """
+
+    f = open(filename)
+    lines = f.readlines()
+
+    nobs = int(lines[0])
+    nwave = float(lines[1])
+    line3 = (lines[2]).split(' ')
+    ntime = int(line3[0])
+    t_i = float(line3[1])
+    t_f = float(line3[2])
+
+    cos_theta = np.linspace(0, 1, nobs)  # 11 viewing angles
+    phase = np.linspace(t_i, t_f, ntime)  # epochs
+
+    file_ = np.genfromtxt(filename, skip_header=3)
+
+    wave = file_[0:int(nwave),0]
+    flux = []
+    for i in range(int(nobs)):
+        flux.append(file_[i*int(nwave):i*int(nwave)+int(nwave),1:])
+    flux = np.array(flux).T
+
+    phase = np.linspace(t_i, t_f, len(flux.T[0][0]))  # epochs
+
+    return phase, wave, cos_theta, flux
+
+class AngularTimeSeriesSource(sncosmo.Source):
+    """A single-component spectral time series model.
+        The spectral flux density of this model is given by
+        .. math::
+        F(t, \lambda) = A \\times M(t, \lambda, \cos_\theta)
+        where _M_ is the flux defined on a grid in phase and wavelength
+        and _A_ (amplitude) is the single free parameter of the model. The
+        amplitude _A_ is a simple unitless scaling factor applied to
+        whatever flux values are used to initialize the
+        ``TimeSeriesSource``. Therefore, the _A_ parameter has no
+        intrinsic meaning. It can only be interpreted in conjunction with
+        the model values. Thus, it is meaningless to compare the _A_
+        parameter between two different ``TimeSeriesSource`` instances with
+        different model data.
+    Parameters
+    ----------
+    phase : `~numpy.ndarray`
+        Phases in days.
+    wave : `~numpy.ndarray`
+        Wavelengths in Angstroms.
+    flux : `~numpy.ndarray`
+        Model spectral flux density in arbitrary units.
+        Must have shape `(num_phases)`.
+    zero_before : bool, optional
+        If True, flux at phases before minimum phase will be zeroed. The
+        default is False, in which case the flux at such phases will be equal
+        to the flux at the minimum phase (``flux[0, :]`` in the input array).
+    zero_after : bool, optional
+        If True, flux at phases after minimum phase will be zeroed. The
+        default is False, in which case the flux at such phases will be equal
+        to the flux at the maximum phase (``flux[-1, :]`` in the input array).
+    cos_theta : `~numpy.ndarray`
+        cosine of viewing angle
+    name : str, optional
+        Name of the model. Default is `None`.
+    version : str, optional
+        Version of the model. Default is `None`.
+    """
+
+    _param_names = ['amplitude', 'theta']
+    param_names_latex = ['A', r'\theta']
+
+    def __init__(self, phase, wave, cos_theta, flux, zero_before=False, zero_after=False, name=None,
+                 version=None):
+        self.name = name
+        self.version = version
+        self._phase = phase
+        self._wave = wave
+        self._cos_theta = cos_theta
+        self._flux_array = flux
+        self._parameters = np.array([1., 0.])
+        self._current_theta = 0.
+        self._zero_before = zero_before
+        self._zero_after = zero_after
+        self._set_theta()
+
+    def _set_theta(self):
+        logflux_ = np.zeros(self._flux_array.shape[:2])
+        for k in range(len(self._phase)):
+            adding = 1e-10 # Here we add 1e-10 to avoid problems with null values
+            f_tmp = Spline2d(self._wave, self._cos_theta, np.log(self._flux_array[k]+adding),
+                             kx=1, ky=1)
+            logflux_[k] = f_tmp(self._wave, np.cos(self._parameters[1]*np.pi/180)).T
+
+        self._model_flux = Spline2d(self._phase, self._wave, logflux_, kx=1, ky=1)
+
+
+        self._current_theta = self._parameters[1]
+
+    def _flux(self, phase, wave):
+        if self._current_theta != self._parameters[1]:
+            self._set_theta()
+        f = self._parameters[0] * (np.exp(self._model_flux(phase, wave)))
+        if self._zero_before:
+            mask = np.atleast_1d(phase) < self.minphase()
+            f[mask, :] = 0.
+        if self._zero_after:
+            mask = np.atleast_1d(phase) > self.maxphase()
+            f[mask, :] = 0.
+        return f
+
 phase, wave, cos_theta, flux = model_tools.read_possis_file(
                 "nsns_nph1.0e+06_mejdyn0.020_mejwind0.130_phi30.txt")
 model= sc.Model(AngularTimeSeriesSource(phase=phase, wave=wave, flux=flux, cos_theta=cos_theta
