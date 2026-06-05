befea0245427596ebe54418cec3e9cf8f45186b8
AntoineGillesLordet <antoine.gilleslordet@fysik.su.se>
2026-06-05 15:34:04 +0200

    Kwargs handling allowing to properly update the model parameters

--- src/skysurvey/target/core.py
+++ src/skysurvey/target/core.py
@@ -1103,7 +1103,8 @@ class Target( object ):
             from modeldag import ModelDAG
             current_model_dict = self.model.model
             drawn_model = ModelDAG( current_model_dict | model, obj=self)            
-            
+        
+        updated_kwargs = dict()
         # => tstart, tstop format
         if type(tstart) is str:
             tstart = time.Time(tstart).mjd
@@ -1139,7 +1140,7 @@ class Target( object ):
         key_redshift = drawn_model.get_func_with_args("zmax")
         for zkey in key_redshift:
             if zmax is not None:
-                kwargs.setdefault(zkey, {}).update({"zmax": zmax})
+                updated_kwargs.setdefault(zkey, {}).update({"zmax": zmax})
             
             elif nyears is not None:
                 zmax = self.get_model_parameter(zkey, "zmax", None, model=drawn_model)
@@ -1150,7 +1151,7 @@ class Target( object ):
         for zkey in key_redshift:
             # note: Why condition "on redshift" ?
             if zmin is not None and "redshift" in self.model.model:
-                kwargs.setdefault(zkey, {}).update({"zmin": zmin})
+                updated_kwargs.setdefault(zkey, {}).update({"zmin": zmin})
             
             elif nyears is not None:
                 zmin = self.get_model_parameter(zkey, "zmin", None, model=drawn_model)
@@ -1159,7 +1160,7 @@ class Target( object ):
             if type( tstop ) is str:
                 tstop = time.Time(tstop).mjd
 
-            kwargs.setdefault("t0", {}).update({"high": tstop})
+            updated_kwargs.setdefault("t0", {}).update({"high": tstop})
 
         #
         # time range
@@ -1168,9 +1169,9 @@ class Target( object ):
             if type( tstart ) is str:
                 tstart = time.Time(tstart).mjd
                 
-            kwargs.setdefault("t0",{}).update({"low": tstart})
+            updated_kwargs.setdefault("t0",{}).update({"low": tstart})
             if tstop is None and nyears is None: # do 1 year by default
-                kwargs.setdefault("t0",{}).update({"high": tstart+365.25})
+                updated_kwargs.setdefault("t0",{}).update({"high": tstart+365.25})
                 
         # tstart is None, then what ?
         elif tstop is not None and nyears is not None:
@@ -1191,7 +1192,7 @@ class Target( object ):
                 warnings.warn("skyarea given but no model have skyarea as parameters. This is ignored.")
             
             for k in param_affected:
-                kwargs.setdefault(k, {}).update({"skyarea": skyarea})
+                updated_kwargs.setdefault(k, {}).update({"skyarea": skyarea})
 
         #
         # Size
@@ -1213,7 +1214,7 @@ class Target( object ):
                 f_area = 1
             
             # redefine timing given nyears
-            kwargs.setdefault("t0", {}).update({"low": tstart, "high": tstart + 365.25*nyears})
+            updated_kwargs.setdefault("t0", {}).update({"low": tstart, "high": tstart + 365.25*nyears})
 
             if zmin is None:
                 zmin = 0
@@ -1224,6 +1225,7 @@ class Target( object ):
             size = int(ntarget_per_year * nyears * f_area)
             
         # actually draw the data
+        kwargs.update(updated_kwargs)
         data = drawn_model.draw(size=size, **kwargs)
         
         # patch the missing `amplitude` back to .data
@@ -1242,7 +1244,8 @@ class Target( object ):
             data = data.astype( {k: str(v).replace("64","32") for k, v in data.dtypes.to_dict().items()})
             self.set_data(data)
             # since this is inplace, let's update stored model kwargs
-            self.update_model_parameter(**kwargs)
+            
+            self.update_model_parameter(**updated_kwargs)
             
         return data
 
